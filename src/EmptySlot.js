45cfeae853162c7f88f0425c6c5b09e728f26ab4
Daniel Fernandez <mdanif9728@gmail.com>
2024-06-05 20:49:57 -0700

    first coomit

---
+++ src/EmptySlot.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function EmptySlot(params) {
+  return (
+    <div>
+      <h3>Empty Slot</h3>
+    </div>
+  )
+}
+
+export default EmptySlot
---
+++ src/MoleContainer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// Components
+import Mole from "./Mole";
+
+function MoleContainer(params) {
+  return (
+    <div>
+      <h2>Mole Container</h2>
+      <Mole />
+    </div>
+  )
+}
+
+export default MoleContainer
